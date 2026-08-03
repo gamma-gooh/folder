3614a333127eab441ef5a06bfeb149bce789bcec
Gamma <balabenlucas7@gmail.com>
2026-08-03 16:36:04 -0300

    cu3

---
+++ extras/catalogo.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menuBtn");
+    const menu = document.getElementById("menu");
+
+    menuBtn.addEventListener("click", function () {
+        menu.classList.toggle("aberto");
+    });
+});
